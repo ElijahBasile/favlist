6bd04e2e2aa69ba46fbd3a5d16208f0b454ef74f
Elijah Basile <berlinboy9@yahoo.com>
2018-05-17 10:47:49 -0400

    creates an app function that handled the submit

--- favlist/index.js
+++ favlist/index.js
@@ -1,15 +1,26 @@
 const app = {
     init: function(formSelector) {
+        this.max = 0
+
         document
             .querySelector(formSelector)
-            .addEventListener('submit', this.handleSubmit)
+            .addEventListener('submit', ev => {
+                ev.preventDefault()
+                this.handleSubmit(ev)
+            })
     },
 
     handleSubmit: function(ev) {
         ev.preventDefault()
         const p = ev.target
-        console.log(p.placeName.value)
+        const place = {
+            id: ++this.max,
+            name: p.placeName.value,
+        }
+        console.log(place)
+        p.reset()
     },
+
 }
 
 app.init('#placeForm')
