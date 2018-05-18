c1aecc2ad7f7ec39144c867bc8dbad7cf1ea0c01
Elijah Basile <berlinboy9@yahoo.com>
2018-05-18 12:33:09 -0400

    make working buttons

--- favlist/README.md
+++ favlist/README.md
@@ -1,3 +1,30 @@
 #Places in Paris
 
 This is the list project for Xtern Bootcamp
+
+## Day 4 homework
+
+*make the _delete_ button **work**, if you haven't already.
+*make the _fav_ button change the style of  the list item in some way.
+
+###Bonus Credit
+
+*Make the fav button also change the oject in the array. For example:
+
+```js
+{
+    name:'GI Joe',
+    fav: true,
+}
+```
+
+###Super Mega Bonus Credit
+
+*Add buttons to move the flick up and down the list
+*Make the buttons also change their place in the array
+
+###Super Mega Bonus Credit Hyper Fighting
+
+*Allow the user to edit the names of the lficks in the list after they're added.
+(Would it be nice if we could make that spans text content !"content editible"!)
+and in their array
--- favlist/index.js
+++ favlist/index.js
@@ -3,7 +3,7 @@ const app = {
         this.places = []
         this.max = 0
         this.list = document.querySelector(selectors.listSelector)
-
+        this.template = document.querySelector(selectors.templateSelector)
         document
             .querySelector(selectors.formSelector)
             .addEventListener('submit', ev => {
@@ -12,10 +12,56 @@ const app = {
             })
     },
 
+
     renderListItem(place) {
-        const item = document.createElement('li')
+        const item = this.template.cloneNode(true)
+        item.classList.remove('template')
         item.dataset.id = place.id
-        item.textContent = place.name
+        item
+            .querySelector('.placeName')
+            .textContent = place.name
+        item.querySelector('.alert').addEventListener('click', ev => {
+            const del = ev.target.parentElement.parentElement
+            del.remove()
+            this.places.splice(this.places.indexOf(place),1)
+        })
+        item.querySelector('.warning').addEventListener('click', ev => {
+            const fav = ev.target.parentElement.parentElement
+            if (place.fav === 'false') {
+                  fav.style.backgroundColor = 'gold'
+                place.fav = "true"
+            } else {
+                fav.style.backgroundColor = 'white'
+                    place.fav = "false"
+            }
+        })
+        item.querySelector('.moveup').addEventListener('click', ev => {
+            const item = ev.target.parentElement.parentElement
+            if (item.previousSibling) {
+                item.parentNode.insertBefore(item, item.previousSibling)
+                const temp = this.places[-1+this.places.indexOf(place)]
+                this.places[-1+this.places.indexOf(place)] = this.places[this.places.indexOf(place)]
+                this.places[this.places.indexOf(place)] = temp
+            }
+        })
+        item.querySelector('.movedown').addEventListener('click', ev => {
+            const item = ev.target.parentElement.parentElement
+            if (item.nextSibling) {
+                item.parentNode.insertBefore(item, item.nextSibling.nextSibling);
+                const temp = this.places[this.places.indexOf(place)]   
+                this.places[this.places.indexOf(place)] = this.places[1+(this.places.indexOf(place))]
+                this.places[1+this.places.indexOf(place)].nextSibling = temp
+            }
+        })
+        item.querySelector('.edit').addEventListener('click', ev => {
+            const item = ev.target.parentElement.parentElement.querySelector('.placeName')
+            item.contentEditable = (item.contentEditable === "false")
+        })
+        item.querySelector('.placeName').addEventListener('keyup', ev => {
+            debugger
+            this.places[this.places.indexOf(place)].name = ev.target.textContent
+        })
+
         return item
     },
 
@@ -25,11 +71,14 @@ const app = {
         const place = {
             id: ++this.max,
             name: p.placeName.value,
+            fav: "false",
         }
         this.places.unshift(place)
 
         const item = this.renderListItem(place)
         this.list.insertBefore(item, this.list.firstElementChild)
+        
+
 
         p.reset()
     },
@@ -39,4 +88,6 @@ const app = {
 app.init({
     formSelector: '#placeForm',
     listSelector: '#placeList',
+    templateSelector: '.place.template',
+    deleteSelector: '.button.alert',
 })
